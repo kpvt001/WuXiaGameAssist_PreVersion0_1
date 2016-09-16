49df0dcece31d78b24f2ffd5b5e3aac537c5c2ce
kpvt001 <kpvt001@qq.com>
2016-09-17 03:06:22 +0800

    add wuxia time config

---
+++ TimeClockLite/WuXiaTimeConfig.cpp
@@ -0,0 +1,6 @@
+#include "WuXiaTimeConfig.h"
+
+WuXiaTimeConfig::WuXiaTimeConfig()
+{
+
+}
---
+++ TimeClockLite/WuXiaTimeConfig.h
@@ -0,0 +1,11 @@
+#ifndef WUXIATIMECONFIG_H
+#define WUXIATIMECONFIG_H
+
+
+class WuXiaTimeConfig
+{
+public:
+    WuXiaTimeConfig();
+};
+
+#endif // WUXIATIMECONFIG_H
