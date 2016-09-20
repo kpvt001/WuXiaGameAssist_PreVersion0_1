febde2d31e0daaf54ab8d0d222d173fdd903e945
kpvt001 <kpvt001@qq.com>
2016-09-21 01:25:58 +0800

    modify version function

--- TimeClockLite/Version.h
+++ TimeClockLite/Version.h
@@ -5,7 +5,7 @@
 
 static const int version_major = 0;
 static const int version_minor = 1;
-static const int version_release = 1;
+static const int version_release = 2;
 static const int version_build = 
 #include "build_number"
         ;
@@ -14,7 +14,7 @@ static const char *version_type = "Alpha";
 
 static QString VersionString_1()
 {
-    return QString("Version: %1.%2.%3-build %4 %5").arg(version_major)
+    return QString("v%1.%2%3 build %4 %5").arg(version_major)
             .arg(version_minor)
             .arg(version_release)
             .arg(version_build)
