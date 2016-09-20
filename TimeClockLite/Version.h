2e61327aa36f71f8490926fe8d2ab04625f9485a
kpvt001 <kpvt001@qq.com>
2016-09-21 01:47:27 +0800

    modify version string in normal mode

--- TimeClockLite/Version.h
+++ TimeClockLite/Version.h
@@ -2,6 +2,7 @@
 #define VERSION_H
 
 #include <QString>
+#include "Settings.h"
 
 static const int version_major = 0;
 static const int version_minor = 1;
@@ -14,11 +15,16 @@ static const char *version_type = "Alpha";
 
 static QString VersionString_1()
 {
-    return QString("v%1.%2%3 build %4 %5").arg(version_major)
-            .arg(version_minor)
-            .arg(version_release)
-            .arg(version_build)
-            .arg(version_type);
+    if(Settings::Global().IsTestMode())
+        return QString("v%1.%2%3 build %4 %5").arg(version_major)
+                .arg(version_minor)
+                .arg(version_release)
+                .arg(version_build)
+                .arg(version_type);
+    else
+        return QString("v%1.%2%3").arg(version_major)
+                .arg(version_minor)
+                .arg(version_release);
 }
 
 #endif // !VERSION_H
