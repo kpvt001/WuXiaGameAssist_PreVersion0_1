8f85cdbf587c67bc9f5788a57e242ad3d30d08ad
kpvt001 <kpvt001@qq.com>
2016-10-20 04:26:16 +0800

    正式确定版本号。 version 1.0.4-dev

--- WuXiaAnswerLite/Version.h
+++ WuXiaAnswerLite/Version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 const int kVersionMajor = 1;
-const int kVersionMinor = 1;
+const int kVersionMinor = 0;
 const int kVersionRevision = 4;
 const char *kVersionTypeString = "Dev";
 
