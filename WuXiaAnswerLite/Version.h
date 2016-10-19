1c039348fbf7fe343a51d9fb317e0cbdb072a77f
kpvt001 <kpvt001@qq.com>
2016-10-20 02:26:33 +0800

    update version

--- WuXiaAnswerLite/Version.h
+++ WuXiaAnswerLite/Version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 const int kVersionMajor = 1;
-const int kVersionMinor = 0;
-const int kVersionRevision = 2;
+const int kVersionMinor = 1;
+const int kVersionRevision = 0;
 const char *kVersionTypeString = "Dev";
 
 #endif
